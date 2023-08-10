4273654cb251bf6c7c724b9b9c578d1e1be91861
Myro <myde.mercado.up@phinmaed.com>
2023-08-10 20:25:04 +0800

    final touches

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,13 @@
-import React from 'react'
+import { Image } from "react-bootstrap";
 
-const Footer = () => {
-  return (
-    <div className='w-100 px-2 text'>
-      <hr />
-      <p>myde.mercado.up@phinmaed.com</p>
+const Footer = ()=> {
+  return(
+    <div className=" d-flex flex-column justify-content-center align-items-center bg-dark text-white py-5 py-1 text-center">
+        <Image src="/logo.png" width={40}/>
+        <h3 className="display-5 fw-bold">PHINMA University of Pangasinan</h3>
+        <p className="fs-6">Colllege of Information Technology</p>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,47 +1,65 @@
 import React from 'react';
-import { Container,  Row, Col } from 'react-bootstrap';
-import { GrWorkshop } from "react-icons/gr";
-import Projects from '../components/Projects';
-import { motion, AnimatePresence } from "framer-motion";
+import { Carousel, Image, Container,  Row, Col } from 'react-bootstrap';
 
 
 const Work = () => {
   return (
-    <AnimatePresence>
-      <motion.div
-        initial={{ opacity:0, y:15}}
-        animate={{ opacity:1, y:0}}
-        transition={{ ease: "easeOut", duration: 2}}>
-        <Container>
-          <Row className='flex align-items-center justify-content-center'>
-          <Col>
-            <h2 className='display-1 fw-bold'>
-              <GrWorkshop/>
-              My Work
-            </h2>
-            <p>Here are some of the projects I've worked on:</p>
-            <ul>
-              <li>
-                <h4 className='fw-bold'>3D Porsche Render</h4>
-                <p>Blender</p>
-              </li>
-              <li>
-                <h4 className='fw-bold'>Video Project Edit</h4>
-                <p>Adobe After Effects, Adobe Premiere Pro, Wondershare Filmora 9</p>
-              </li>
-              <li>
-                <h4 className='fw-bold'>Kawasaki Website</h4>
-                <p>HTML, CSS</p>
-              </li>
-            </ul>
-          </Col>
-          <Col>
-            <Projects />
-          </Col>
-        </Row>
-        </Container>
-      </motion.div>
-    </AnimatePresence>
+    <Container>
+      <Row>
+      <Col>
+        <h2 className='display-1 fw-bold'>
+           My Work
+        </h2>
+        <p>Here are some of the projects I've worked on:</p>
+        <ul>
+          <li>
+            <h4>3D Porsche Render</h4>
+            <p>Blender</p>
+          </li>
+          <li>
+            <h4>Video Edit</h4>
+            <p>Adobe After Effects, Adobe Premiere Pro, Wondershare Filmora</p>
+          </li>
+          <li>
+            <h4>Kawasaki Motor Website</h4>
+            <p>HTML, CSS, JS</p>
+          </li>
+        </ul>
+      </Col>
+      <Col>
+      <div style={{minWidth:280}}>
+      <Carousel>
+        <Carousel.Item>
+           <div style={{maxHeight:500}}>
+            <Image src="/portfolio-hover1.gif" className="w-100 h-100 object-cover"/>
+           </div>
+          <Carousel.Caption>
+            <h3>3D Porsche Render</h3>  
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div style={{maxHeight:500}}>
+            <Image src="/portfolio-hover2.gif" className="w-100 h-100 object-cover"/>
+           </div>
+          <Carousel.Caption>
+            <h3>Video Edit Project for PSU UC Students</h3>  
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          
+          <div style={{maxHeight:500}}>
+            <Image src="/portfolio-hover3.gif" className="w-100 h-100 object-cover"/>
+           </div>
+          <Carousel.Caption>
+            <h3>Kawasaki Motors Website</h3>
+           
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+      </div>
+      </Col>
+    </Row>
+    </Container>
   );
 };
 
